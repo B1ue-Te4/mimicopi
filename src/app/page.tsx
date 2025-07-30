ea3aabf3d0a7092efa011f3279cd4768e4eb716a
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-07-30 12:26:21 +0000

    cmt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,22 @@
 'use client'
 
-import YouTube, { YouTubePlayer } from 'react-youtube'
+import ReactPlayer from 'react-player'
 import { useRef, useState } from 'react'
 import { RewindIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 export default function Page() {
 
   console.log('page')
 
-  const [url, setUrl] = useState('')
-  const [videoId, setVideoId] = useState('')
+  const [videoUrl, setVideoUrl] = useState('https://youtu.be/ylXk1LBvIqU?si=oQAJMAhI-num1Xk0')
+  // const playerRef = useRef<ReactPlayer>(null)
 
-  const playerRef = useRef<YouTubePlayer | null>(null)
-
-  const onReady = (e: { target: YouTubePlayer }) => {
-    playerRef.current = e.target
-  }
-
-  const rewind = () => {
-    const current = playerRef.current?.getCurrentTime?.() || 0
-    playerRef.current?.seekTo(current - 5, true)
-  }
-
-  const extractVideoId = (url: string): string | undefined => {
-    const match = url.match(
-      /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})(?:\S+)?$/
-    )
-    return match?[1] : undefined
-  }
-
-  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('handle')
-    const value = e.target.value
-    setUrl(value)
-    const id = extractVideoId(value)
-    console.log('extracted id:', id)
-    if (id) setVideoId(id)
-  }
+  //const rewind = () => {
+  //  const current = playerRef.current?.getCurrentTime?.() || 0
+  //  playerRef.current?.seekTo(current - 5, true)
+  //}
 
   return (
     <main>
@@ -47,11 +24,11 @@ export default function Page() {
         type="url"
         id="youtubeurl"
         placeholder="YoutubeURL"
-        value={url}
-        onChange={handleUrlChange}
+        value={videoUrl}
+        onChange={(e) => setVideoUrl(e.target.value)}
       />
-      <YouTube videoId={videoId} onReady={onReady} />
-      <Button onClick={rewind}>
+      <ReactPlayer src={videoUrl} />
+      <Button>
         <RewindIcon />5sec
       </Button>
     </main>
