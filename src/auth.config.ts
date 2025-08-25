4bf60edb19e61b26c65b9877573ea125513f8782
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-08-25 13:31:03 +0000

    auth.ts

---
+++ src/auth.config.ts
@@ -0,0 +1,20 @@
+import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+
+const authConfig: NextAuthConfig = {
+  providers: [
+    Google,
+  ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user && account?.id_token) {
+        token.idToken = account?.id_token;
+      }
+      return token;
+    },
+    async session({ token, session }) {
+      session.idToken = token.idToken;
+      return session;
+    },
+  },
+};
