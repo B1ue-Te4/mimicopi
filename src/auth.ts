fb72ea6b888fe3ed634d4f9ce9e7dd47d6abb1d9
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-09-07 07:12:53 +0000

    auth setting

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,8 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
 
-export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
+const authConfig: NextAuthConfig = {
+  providers: [
+     Google, 
+  ],
+};
